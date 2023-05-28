3ef4ff4028f4e0770b2420480500397908984830
ChauDucGiang <chau.ducgiang@gmail.com>
2023-05-28 21:57:24 +0700

    ENEMY
    ENEMY

--- CustomFramework/Game/EnemyRifleMan.cpp
+++ CustomFramework/Game/EnemyRifleMan.cpp
@@ -5,7 +5,7 @@ EnemyRifleMan::EnemyRifleMan()
 	animations[STANDING] = new Animation(ENEMY, 0, 0);
 	animations[RUNNING] = new Animation(ENEMY, 0, 2, DEFAULT_TPF << 1);
 	tag = ENEMY;
-	type = SOLIDER;
+	type = RIFLEMAN;
 	width = ENEMY_SWORDMAN_WIDTH;
 	height = ENEMY_SWORDMAN_HEIGHT;
 	speed = ENEMY_SWORDMAN_SPEED;
